510b946c6a0fd5b7a0421831e8df4c6906682b6c
compuYael <aguilar.eduardo32@yahoo.com>
2020-02-13 21:33:10 -0600

    interacion3

---
+++ starter-code/src/Button.js
@@ -0,0 +1,9 @@
+import React from "react";
+const Buttons = props => {
+    return (
+        <button className={props.class}>Button 1</button>
+        <button className={props.class}>Button 2</button>
+     )
+}
+
+export default Buttons
